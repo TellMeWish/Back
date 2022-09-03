9483f9ad41f9ca7f544a3116f66379d3fe551fde
MINGYU PARK <54715532+alsrb2026@users.noreply.github.com>
2022-09-03 13:27:58 +0900

    Update SecurityConfig.java
    postList 권한제한 없앰

--- src/main/java/jpabook/jpashop/config/SecurityConfig.java
+++ src/main/java/jpabook/jpashop/config/SecurityConfig.java
@@ -99,6 +99,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/hello").permitAll()
                 .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/signup").permitAll()
+                .andMatchers("/postList").permitAll()
 
                 .anyRequest().authenticated() //나머지 요청들은 인증해야함
 
